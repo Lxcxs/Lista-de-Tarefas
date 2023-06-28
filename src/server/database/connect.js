6935514087f52b3bc6921b6b76a2ac0819f5429b
Felipe Las Casas <felipelascasas7@gmail.com>
2023-06-28 16:44:52 -0300

    inputs optimization

--- src/server/database/connect.js
+++ src/server/database/connect.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
 const connectToDatabase = async () => {
+    //inserir url do mongodb
     await mongoose
-    .connect(`mongodb+srv://lvcxs:${process.env.MONGODB_PASSWORD}@nodelearn.mvxptr0.mongodb.net/`)
+    .connect()
     .then(console.log("Conexão efetuada com sucesso!"))
     .catch(error => {
         console.log("Ocorreu um erro ao realizar a conexão! ", error)
